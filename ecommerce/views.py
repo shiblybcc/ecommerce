7dabb6b4e29d92a1a8881e151743392e07b46988
bcc <shiblyxp@yahoo.com>
2019-07-31 22:40:37 +0200

    t18 - registration and authentication

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 
-from .forms import ContactForm, LoginForm
+from .forms import ContactForm, LoginForm, RegisterForm
 
 def home_page(request):
 
@@ -42,14 +42,20 @@ def login_page(request):
         if user is not None:
             login(request, user)
             # print(request.user.is_authenticated)
-            return redirect('/login')
+            return redirect('/')
         else:
             print('Error')
     return render(request, 'auth/login.html', context)
 
-
+User = get_user_model()
 def register_page(request):
-    form = LoginForm(request.POST or None)
+    form = RegisterForm(request.POST or None)
+    context = {
+        'form': form
+    }
     if form.is_valid():
-        print(form.cleaned_data)
-    return render(request, 'auth/register.html', {})
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        new_user = User.objects.create_user(username, email, password)
+    return render(request, 'auth/register.html', context)
