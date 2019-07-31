c0cc5f5ddb1d4d613c6ca09545b4cbe92d4a4ff7
bcc <shiblyxp@yahoo.com>
2019-07-31 19:57:37 +0200

    t13 - nothing here

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -6,3 +6,13 @@ from django.shortcuts import render,redirect
 def home_page(request):
 
     return render(request, 'home_page.html', {})
+
+
+def about_page(request):
+
+    return render(request, 'home_page.html', {})
+
+
+def contact_page(request):
+
+    return render(request, 'home_page.html', {})
