033575d21474c90ede12aa4937c150f8e8c6771f
bcc <shiblyxp@yahoo.com>
2019-08-01 20:38:02 +0200

    t24 - List view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+class ProductListView(ListView):
+    qs = Product.objects.all()
+    template_name = 'products/list.html'
+
+    def get_context_data(self, *args, object_list=None, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        return context
+
+# above class based view and below function based view are same
+# def product_list_view(request):
+#     qs = Product.objects.all()
+#     context = {
+#         'object_list': qs
+#     }
+#     return render(request, 'product/product_list_view.html', context)
