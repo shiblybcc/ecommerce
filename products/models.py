19bbdfb53a5a56b844f775b0e7d754242ade8550
bcc <shiblyxp@yahoo.com>
2019-08-03 12:06:32 +0200

    t37 - reverse urls

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@ import random
 import os
 from django.db import models
 from django.db.models.signals import pre_save, post_save
+from django.urls import reverse
 
 from ecommerce.utils import unique_slug_generator
 
@@ -15,8 +16,8 @@ def get_filename_ext(filename):
 def upload_image_path(instance, filename):
     new_filename = random.randint(1, 3910873486595)
     name, ext = get_filename_ext(filename)
-    final_filename = '{new_filename}{ext}'.format(new_filename, ext)
-    return 'products/{new_filename}/{final_filename}'.format(new_filename, final_filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return 'products/{new_filename}/{final_filename}'.format(new_filename=new_filename, final_filename=final_filename)
 
 
 class ProductQuerySet(models.query.QuerySet):
@@ -56,7 +57,8 @@ class Product(models.Model):
     objects = ProductManager()
 
     def get_absolute_url(self):
-        return '/products/{slug}'.format(slug=self.slug)
+        # return '/products/{slug}'.format(slug=self.slug)
+        return reverse('products:detail', kwargs={'slug': self.slug})
 
     def __str__(self):
         return self.title
--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 
 from .views import (ProductListView, ProductFeaturedDetailSlugView)
 
+app_name = 'products'
+
 urlpatterns = [
     url(r'^$', ProductListView.as_view()),
-    url(r'^(?P<slug>[\w-]+)/$', ProductFeaturedDetailSlugView.as_view()),
+    url(r'^(?P<slug>[\w-]+)/$', ProductFeaturedDetailSlugView.as_view(), name='detail'),
 ]
