fd9d15f4fe9a2dc91f443db147f9df71213f4e48
bcc <shiblyxp@yahoo.com>
2019-08-04 21:59:21 +0200

    t65 - cart update: cart model manager

--- carts/models.py
+++ carts/models.py
@@ -6,12 +6,24 @@ from products.models import Product
 User = settings.AUTH_USER_MODEL
 
 
+class CartManager(models.Manager):
+
+    def new(self, user=None):
+        new_user = None
+        if user is not None:
+            if user.is_authenticated:
+                new_user = user
+        return self.model.objects.create(user=new_user)
+
+
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(default=0.0, max_digits=100, decimal_places=2)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = CartManager()
 
     def __str__(self):
         return str(self.id)
--- carts/views.py
+++ carts/views.py
@@ -10,14 +10,13 @@ def cart_create(user=None):
 
 def cart_home(request):
     cart_id = request.session.get("get_id", None)
-    # if cart_id is None:
-    #     cart = cart_create()
-    #     request.session['cart_id'] = cart.id
-    # else:
     qs = Cart.objects.filter(id=cart_id)
     if qs and qs.count() == 1:
         cart = qs.first()
+        if request.user.is_authenticated() and cart.user is None:
+            cart.user = request.user
+            cart.save()
     else:
-        cart = cart_create()
+        cart = Cart.objects.new(user=request.user)
         request.session['cart_id'] = cart.id
     return render(request, 'carts/home.html', {})
