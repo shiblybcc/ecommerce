6e788361bbce79db4a4e47258e8ac7c107746507
bcc <shiblyxp@yahoo.com>
2019-08-04 09:21:56 +0200

    t54 - search results and view

--- search/views.py
+++ search/views.py
@@ -5,12 +5,13 @@ from products.models import Product
 
 # Create your views here.
 
+
 class SearchProductView(ListView):
-    template_name = 'products/list.html'
+    template_name = 'search/view.html'
 
     def get_queryset(self, *args, **kwargs):
         request = self.request
-        query = request.Get.get('q')
+        query = request.GET.get('q', "")
         if query is not None:
             return Product.objects.filter(title__icontains=query)
         return Product.objects.none()
