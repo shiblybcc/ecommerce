e17b27eb588d1fe1941b2821e0c847ea452a8ae7
bcc <shiblyxp@yahoo.com>
2019-08-04 16:14:32 +0200

    t55 - working search field

--- search/urls.py
+++ search/urls.py
@@ -5,5 +5,5 @@ from .views import SearchProductView
 app_name = 'search'
 
 urlpatterns = [
-    url(r'^$', SearchProductView.as_view(), name='list'),
+    url(r'^$', SearchProductView.as_view(), name='query'),
 ]
